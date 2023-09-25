454dba86936fcc4365b61ffcf5a1164d4ffb1b14
Khalid Benjelloun <khalidmailing@gmail.com>
2023-09-25 03:22:24 +0100

    edit

--- _printf.c
+++ _printf.c
@@ -1,24 +1,22 @@
 #include "main.h"
 
-void print_buffer(char buffer[], int *buff_ind);
+void	print_buffer(char buffer[], int *buff_ind);
 
 /**
  * _printf - Printf function
  * @format: format.
  * Return: Printed chars.
  */
-int _printf(const char *format, ...)
+int	_printf(const char *format, ...)
 {
+	va_list	list;
+	char	buffer[BUFF_SIZE];
+
 	int i, printed = 0, printed_chars = 0;
 	int flags, width, precision, size, buff_ind = 0;
-	va_list list;
-	char buffer[BUFF_SIZE];
-
 	if (format == NULL)
 		return (-1);
-
 	va_start(list, format);
-
 	for (i = 0; format && format[i] != '\0'; i++)
 	{
 		if (format[i] != '%')
@@ -37,18 +35,15 @@ int _printf(const char *format, ...)
 			precision = get_precision(format, &i, list);
 			size = get_size(format, &i);
 			++i;
-			printed = handle_print(format, &i, list, buffer,
-				flags, width, precision, size);
+			printed = handle_print(format, &i, list, buffer, flags, width,
+					precision, size);
 			if (printed == -1)
 				return (-1);
 			printed_chars += printed;
 		}
 	}
-
 	print_buffer(buffer, &buff_ind);
-
 	va_end(list);
-
 	return (printed_chars);
 }
 
@@ -57,13 +52,9 @@ int _printf(const char *format, ...)
  * @buffer: Array of chars
  * @buff_ind: Index at which to add next char, represents the length.
  */
-void print_buffer(char buffer[], int *buff_ind)
+void	print_buffer(char buffer[], int *buff_ind)
 {
 	if (*buff_ind > 0)
 		write(1, &buffer[0], *buff_ind);
-
 	*buff_ind = 0;
 }
-
-
-
